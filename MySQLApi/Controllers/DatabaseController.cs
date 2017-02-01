4bcbef834d5afca9202ac81b81cb619249b04201
Howard Edidin <hse@msn.com>
2017-02-01 17:37:50 -0600

    fixed

--- MySQLApi/Controllers/DatabaseController.cs
+++ MySQLApi/Controllers/DatabaseController.cs
@@ -13,47 +13,50 @@ namespace MySQLApi.Controllers
 {
     public class DatabaseController : ApiController
     {
-
         /// <summary>
-        /// Gets the records.
+        ///     Gets the records.
         /// </summary>
-        /// <param name="query">The query.</param>
+        /// <param name="query">The SQL query.</param>
         /// <returns></returns>
         /// <exception cref="HttpResponseException"></exception>
         /// <exception cref="HttpResponseMessage"></exception>
+        [HttpGet]
         [SwaggerOperation("GetRecords")]
         [SwaggerResponse(HttpStatusCode.OK)]
         [SwaggerResponse(HttpStatusCode.NotFound)]
         public async Task<dynamic> GetRecords(string query)
         {
-            var con = Connect();
-
-            var list = new List<dynamic>();
-
-
-            try
+            using (var con = Connect())
             {
-                var cmd = new MySqlCommand(query, con);
-                var rdr = await cmd.ExecuteReaderAsync(CommandBehavior.Default, new CancellationToken());
+                var list = new List<dynamic>();
 
+                try
+                {
+                    using (var cmd = new MySqlCommand(query, con))
+                    {
+                        var rdr = await cmd.ExecuteReaderAsync(CommandBehavior.Default, new CancellationToken());
+                        if (!rdr.HasRows)
+                            return new HttpResponseMessage(HttpStatusCode.NotFound)
+                            {
+                                Content = new StringContent("No Records Found")
+                            };
+                        while (rdr.Read())
+                            list.Add(rdr);
 
-                if (!rdr.HasRows) throw new HttpResponseException(new HttpResponseMessage(HttpStatusCode.NotFound));
-                while (rdr.Read())
-                    list.Add(rdr);
-
-                return list;
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e);
-                throw;
+                        return list;
+                    }
+                }
+                catch (Exception e)
+                {
+                    return new HttpResponseMessage(HttpStatusCode.BadRequest) {Content = new StringContent(e.Message)};
+                }
             }
         }
 
         private static MySqlConnection Connect()
         {
             const string ConnStr =
-                "server=138.91.241.48;user=hellboy;database=vitaldev;port=3306;password=qu4gm1r3;";
+                "server=azu-lnx-hydratest;user=hellboy;database=vitaldev;port=3306;password=qu4gm1r3;";
 
             using (var conn = new MySqlConnection(ConnStr))
             {
@@ -64,8 +67,7 @@ namespace MySQLApi.Controllers
                 }
                 catch (Exception e)
                 {
-                    Console.WriteLine(e);
-                    throw;
+                    throw new HttpResponseException(HttpStatusCode.InternalServerError) {Source = e.Message};
                 }
             }
         }
